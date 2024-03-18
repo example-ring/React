ca91d918fa6845626e32f452ad74371955368343
yerangrang <eiren_123@naver.com>
2024-03-18 18:53:33 +0900

    Counter 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-
 import './App.css';
+import { useState } from 'react';
+import Controller from './Component2/Controller';
+import Viewer from "./Component2/Viewer"
 
-import Header from "./component/Header";
-import Body from "./component/Body";
-import Footer from "./component/Footer";
+// import Header from "./component/Header";
+// import Body from "./component/Body";
+// import Footer from "./component/Footer";
 
+
+
+// function App() {
+// return (
+// <div className="App">
+// <Header></Header>
+// <Body></Body>
+// <Footer></Footer>
+// </div>
+// );
+// }
+
+// export default App;
 
 
 function App() {
-return (
-<div className="App">
-<Header></Header>
-<Body></Body>
-<Footer></Footer>
-</div>
-);
+ 
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+ 
+ 
+  return (
+    <div className="App">
+      <h1>Simple Counter Practice</h1>
+      <section>
+        <Viewer count = {count}></Viewer>
+      </section>
+      <section>
+        <Controller handleSetCount = {handleSetCount}></Controller>
+      </section>
+    </div>
+  );
 }
 
 export default App;
