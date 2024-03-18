95b29ae480295bc2c6f7abb7366f2eb1a8ed65ae
yerangrang <eiren_123@naver.com>
2024-03-18 17:00:05 +0900

    페이지 구성 요소 렌더링

---
+++ src/Component/Body.jsx
@@ -0,0 +1,12 @@
+function Body () {
+	const strA = "안녕";
+	const strB = "클레오파트라";
+	const strC = "세상에서 제일 가는 포테이토칩"
+	return (
+	 <div>
+		<h1>body</h1>
+		<h2>{strA + strB + strC}</h2>
+	 </div>	
+	);
+}
+export default Body;
