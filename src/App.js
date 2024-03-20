2091ccb86a886738fad728d1c1306b831dde828b
yerangrang <eiren_123@naver.com>
2024-03-20 20:31:39 +0900

    To do 관리 기능 구현 실습

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import {useRef, useState} from "react";
 import Header from "./TodoProject/Header";
 import TodoEditor from "./TodoProject/TodoEditor";
-import TodoList from "./TodoProject/TodoList";
+import TodoList from './TodoProject/TodoList';
 
 
 
 function App () {
 
-	const [todo, setodo] = useState([]);
+	const [todo, setTodo] = useState([]);
 	const idRef = useRef(3);
 
 	const onCreate = (content) => {
@@ -20,17 +20,45 @@ function App () {
 			isDone: false,
 			createdDate: new Date().getTime(),
 		};
-		setodo([newItem, ...todo]);
+		setTodo([newItem, ...todo]);
 		idRef.current += 1;
 
 	};
+
+const onUpdate = (targetId) => {
+
+	setTodo(
+			
+			todo.map((it) => it.id === targetId ? {...it, isDone: !it.isDone} : it)
+
+			//간결하게 삼항 연산자로 수정
+
+			// (it) => {
+			// 	if (it.id === targetId) {
+			// 		return {
+			// 			...it,
+			// 			isDone: !it.isDone,	
+			// 		};
+			// 	} else {
+			// 		return it;
+			// 	}
+			// }
+			
+		);	
+};
+
+const onDelete = (targetId) => {
+	setTodo(todo.filter((it) => it.id !== targetId));
+
+};
+
 
 	return (
 
 		<div className="App">
 			<Header></Header>
 			<TodoEditor onCreate={onCreate}></TodoEditor>
-			<TodoList todo={todo}></TodoList>
+			<TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete}></TodoList>
 		</div>
 
 	);
--- src/TodoProject/TodoList.jsx
+++ src/TodoProject/TodoList.jsx
@@ -1,15 +1,30 @@
 import "./TodoList.css"
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const TodoList = ({todo}) => {
+const TodoList = ({todo, onUpdate, onDelete }) => {
+
+const [ search, setsearch ] = useState("");
+const onChangeSearch = (e) => {
+	setsearch(e.target.value)
+};
+
+const getSearchResult = () => {
+	return search === ""
+	? todo
+	: todo.filter((it) => it.content.toLowerCase().includes(search.toLowerCase()));
+}
 
 return (
 	<div className="TodoList">
  <h4>♡ Todo List ♥</h4>
- <input className="searchbar" placeholder="검색어 입력"></input>
+ <input 
+	value={search}
+	onChange={onChangeSearch}
+ className="searchbar" placeholder="검색어 입력"></input>
 	<div className="list_wrapper"> 
-	{todo.map((it) => (
-		<TodoItem {...it}></TodoItem>
+	{getSearchResult().map((it) => (
+		<TodoItem key={it.id} {...it} onUpdate={onUpdate} onDelete={onDelete}></TodoItem>
 	))}
 	</div>
 </div>
