881a3179c5cb696e374268adcf715438539d835a
DESKTOP-LED6D3R\박예린 <eiren_123@naver.com>
2024-05-26 21:15:15 +0900

    2: Editor 컴포넌트 (공통 스타일 설정하기)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
-import Editor from "../pages-component/Editor";
 
 
 const Home = () => {
   return (
     <div>
-        <Editor 
-        initData={{
-          date: new Date().getTime(),
-          emotionId: 1,
-          content: "이전에 작성했던 일기",
-        }}
-        onSubmit={() => alert("작성 완료!")} 
-        />
+        Home 페이지입니다.
     </div>
   );
 };
