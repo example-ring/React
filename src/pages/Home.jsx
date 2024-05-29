c31264b789e1883e6cf59ab8dabf66e53794bd58
DESKTOP-LED6D3R\박예린 <eiren_123@naver.com>
2024-05-30 00:23:30 +0900

    Home 페이지 구현하기: 헤더 구현하기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-
+import { useState } from "react"; 
+import Button from "../pages-component/Button";
+import Header from "../pages-component/Header";
 
 const Home = () => {
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const headerTitle = `${pivotDate.getFullYear()}년
+  ${pivotDate.getMonth() + 1}월`;
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() -1));
+  };
   return (
     <div>
-        Home 페이지입니다.
+      <Header
+      title={headerTitle}
+      leftChild={<Button text={"<"} onClick={onDecreaseMonth}></Button>}
+      rightChild={<Button text={">"} onClick={onIncreaseMonth}></Button>}
+      />
     </div>
   );
 };
