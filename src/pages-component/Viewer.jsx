f16d24aacf4ca8fd437f7e089e41035573a05b39
DESKTOP-LED6D3R\박예린 <eiren_123@naver.com>
2024-06-14 22:55:58 +0900

    Diary 페이지 구현하기

--- src/pages-component/Viewer.jsx
+++ src/pages-component/Viewer.jsx
@@ -17,6 +17,13 @@ import { emotionList } from "../util";
                         <div className="emotion_descript">{emotionItem.name}</div>
                     </div>
                 </section>
+
+                <section>
+                    <h4>오늘의 일기</h4>
+                    <div className="content_wrapper">
+                    <p>{content}</p>
+                    </div>
+                </section>
             </div>
         );
 };
